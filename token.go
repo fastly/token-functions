c1bc7148796a387cf0759c8096e95e248a9d6b5c
Simon Wistow <simon@fastly.com>
2014-09-13 11:04:03 -0400

    Make source more idiomatic

--- token.go
+++ token.go
@@ -14,43 +14,45 @@ import (
 
 func main() {
 	encodedkey := "RmFzdGx5IFRva2VuIFRlc3Q="
-	var interval int64 = 60
+	var interval uint64 = 60
 
-	secret, err := base64.StdEncoding.DecodeString(encodedkey)
+	key, err := base64.StdEncoding.DecodeString(encodedkey)
 	if err != nil {
-		log.Fatal("error base64 decoding:", err)
+		log.Fatalf("error base64 decoding: %s", err)
 	}
-	key := []byte(secret)
 
-	number := Int64bytes(time.Now().Unix() / interval)
+	number := Int64bytes(uint64(time.Now().Unix()) / interval)
 	h := hmac.New(sha256.New, key)
-	h.Write([]byte(number))
+	h.Write(number)
 
 	token := base64.StdEncoding.EncodeToString(h.Sum(nil))
 
-	response := FetchUrl("http://token.fastly.com/token")
-	validation := FetchUrl("http://token.fastly.com?" + token)
+	response := FetchURL("http://token.fastly.com/token")
+	validation := FetchURL("http://token.fastly.com?" + token)
 
 	fmt.Println("Your Token:   ", token)
-	fmt.Println("Fastly Token: ", response)
-	fmt.Println("Validation:   ", validation)
+	fmt.Println("Fastly Token: ", string(response))
+	fmt.Println("Validation:   ", string(validation))
 }
 
-func Int64bytes(int int64) []byte {
+func Int64bytes(int uint64) []byte {
 	bytes := make([]byte, 8)
 	binary.LittleEndian.PutUint64(bytes, uint64(int))
 	return bytes
 }
 
-func FetchUrl(url string) string {
+func FetchURL(url string) []byte {
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatal("error fetching url ", url, ": ", err)
+		log.Fatalf("error fetching url %s : %s", url, err)
+	}
+	if resp.StatusCode != 200 {
+		log.Fatalf("error fetching url %s : %s", url, resp.Status)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal("error reading body of http response: ", err)
+		log.Fatalf("error reading body of http response: %s", err)
 	}
-	return string(body)
+	return body
 }
